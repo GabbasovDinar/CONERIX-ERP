80defc2f4f1113f64cff736659d2542d6ed55b2e
Erik Albert Bruder <conerix@googlemail.com>
2016-04-20 02:42:12 +0200

    Added files via upload
    modified ws_favicon

---
+++ ws_favicon_bd/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Website Favicon BRUDER Datentechnik",
+    'version': '1.0.0',
+    'author': 'Erik Bruder',
+    'category': 'Custom',
+    'website': 'http://bruder-datentechnik.de',
+    'description': '''Allows you to change the Favicon,
+from both sides, website, and web''',
+    'depends': ['website'],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True
+}
