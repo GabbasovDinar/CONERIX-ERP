9bceaa0898f2328d1d7328d89f8f18a83467d313
conerix <conerix@googlemail.com>
2017-01-27 05:36:32 +0100

    Partner Wireless

--- partner_wireless/__openerp__.py
+++ partner_wireless/__openerp__.py
@@ -1,35 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "partner_wireless",
-
-    'summary': """
-        Wireless network credentials from partner
-        SSID, KEY, ROUTER, ROUTERPASSWORD""",
-
-    'description': """
-        Safe the wireless networks of your partners
-    """,
-
-    'author': "BRUDER Datentechnik",
-    'website': "http://www.bruder-datentechnik.de",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'sale'],
-
-    # always loaded
+    'name': "Partner Wireless BRUDER Datentechnik",
+    'version': '1.0.0',
+    'author': 'Erik Bruder',
+    'category': 'Custom',
+    'website': 'http://www.bruder-datentechnik.de',
+    'description': '''Allows you to store the wireless credentials,
+    from partner''',
+    'depends': ['sale'],
     'data': [
-        # 'security/ir.model.access.csv',
         'partner_wireless.xml',
-    ],
-    'qweb':[
-    ],
-    "auto_install": false
-    "installable": True,
+        ],
+    'installable': True
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
