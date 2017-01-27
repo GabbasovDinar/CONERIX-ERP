2865e13a1b7b522fc28e27317b6fe815c67ecb20
conerix <conerix@googlemail.com>
2017-01-27 05:16:47 +0100

    delete

--- partner_wireless/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class partner_wireless(models.Model):
-#     _name = 'partner_wireless.partner_wireless'
-
-#     name = fields.Char()
